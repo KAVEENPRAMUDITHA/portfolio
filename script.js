5be0fcace9972f54d22df2da39797977a3979a6d
Kaveen Pramuditha <pramudithakaveen51@gmail.com>
2024-11-22 22:35:46 +0530

    FINAL

---
+++ script.js
@@ -0,0 +1,16 @@
+const scriptURL = 'https://script.google.com/macros/s/AKfycbyPhdNO3cWOhfVSxKnjHVDYaQ8vDk_Na3PsLq5J37zlKq6s8fUZVoqClN8mCYOVs8shQw/exec';
+const form = document.forms['submit-to-google-sheet'];
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault(); // Prevent default form submission behavior
+    fetch(scriptURL, { method: 'POST', body: new FormData(form) })
+        .then((response) => {
+            console.log('Success!', response);
+            alert('Your message has been sent successfully!');
+            form.reset(); // Reset form after successful submission
+        })
+        .catch((error) => {
+            console.error('Error!', error.message);
+            alert('There was an error submitting your message. Please try again.');
+        });
+});
