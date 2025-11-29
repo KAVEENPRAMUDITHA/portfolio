2f36706244499b5ac4307e75d293cec828d0d390
Kaveen Pramuditha <pramudithakaveen51@gmail.com>
2025-11-30 00:45:43 +0530

    New look 🎉🫡

---
+++ main.js
@@ -0,0 +1,19 @@
+
+const sections = document.querySelectorAll('section[id]')
+
+function scrollActive(){
+    const scrollY = window.pageYOffset
+
+    sections.forEach(current =>{
+        const sectionHeight = current.offsetHeight
+        const sectionTop = current.offsetTop - 50;
+        sectionId = current.getAttribute('id')
+
+        if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
+            document.querySelector('.navbar-nav a[href*=' + sectionId + ']').classList.add('active')
+        }else{
+            document.querySelector('.navbar-nav a[href*=' + sectionId + ']').classList.remove('active')
+        }
+    })
+}
+window.addEventListener('scroll', scrollActive)
